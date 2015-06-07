ea9fb4361a859829f3cb7d1333813489fe66669f
gabrieletudes <gabrieletudes@gmail.com>
2015-06-07 19:39:42 +0200

    getAuthors to get all my authors and getAuthors to get just an author

--- models/AuthorsModel.php
+++ models/AuthorsModel.php
@@ -8,8 +8,15 @@ SELECT books.id as books_id, genres.id as genres_Id FROM `books` JOIN genres ON 
  class AuthorsModel extends model{
 
  	function getAuthors(){
- 		$sql = 'SELECT authors.name as authors_name,  FROM authors';
+ 		$sql = 'SELECT name, bio, datebirth, datedeath, photo FROM authors';
  		$pdost = $this->cx->query($sql);
  		return $pdost->fetchAll();
  	}
+
+ 	function getAuthor($author_id){
+ 		$sql = 'SELECT name FROM authors WHERE id ='.$author_id;
+ 		$pdost = $this->cx->query($sql);
+ 		return $pdost->fetch();
+ 	}
  }
+
