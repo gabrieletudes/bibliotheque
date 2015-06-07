24232d275bb32fa3cc59c3b83cc2676ce9f91fd1
gabrieletudes <gabrieletudes@gmail.com>
2015-06-07 20:12:41 +0200

    added pageAutheur

---
+++ views/pageAutheur.php
@@ -0,0 +1,21 @@
+<h1>
+	<?php echo $data['author']['author_name'] ;?>
+	<?php echo $data['author']['bio'] ;?>
+</h1>
+
+<div id="contenu">
+	<div class="columne-introduction">
+		 <h1>
+			<?php echo $data['genre']['name'] ;?>
+		</h1>
+		<ul>
+		<?php foreach ($data['books'] as $book): ?>
+			<li><h2><?php echo $book['title'] ?></h2>
+			<img src="./img/<?php echo $book['front_cover'] ?>" alt="">
+			<p><?php echo $book['summary'] ?></p>					
+			</li>
+		<?php endforeach ?>
+		</ul>
+	</div>
+</div>
+
